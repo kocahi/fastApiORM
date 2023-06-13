5b2644f13b1d9a6a3674a8de4c5f1e1b48730194
Enrico Fernando Chai <enrikofernando@gmail.com>
2023-06-13 10:51:43 +0700

    update

--- configs/database.py
+++ configs/database.py
@@ -5,8 +5,6 @@ from configs.config import Settings
 
 check = Settings()
 
-#server = 'nomad2805'
-#database = 'DMSTEST'
 
 database_url = f"mssql+pymssql://{check.db_user}:{check.db_password}@{check.db_server}/{check.db_name}" #MSSQL SERVER
 #database_url = 'mssql+pyodbc://' + server + '/' + database + '?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server'
--- utils/AddEntitiesEngine.py
+++ utils/AddEntitiesEngine.py
@@ -2,10 +2,10 @@ import os
 import importlib
 from configs.database import engine
 
-def addmodels():
-    moduleNamesOST = os.listdir("entities")
-    for moduleName in moduleNamesOST:
-        name, ext = os.path.splitext(moduleName)
-        if (ext == ".py"):
-            module = importlib.import_module("entities." + name)
-            module.Base.metadata.create_all(bind=engine)
+# def addmodels():
+#     moduleNamesOST = os.listdir("entities")
+#     for moduleName in moduleNamesOST:
+#         name, ext = os.path.splitext(moduleName)
+#         if (ext == ".py"):
+#             module = importlib.import_module("entities." + name)
+#             module.Base.metadata.create_all(bind=engine)
